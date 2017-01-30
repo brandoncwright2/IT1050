513f237b59584d69b0419db1db3ea822369da4c1
brandon wright <brandoncwright2@gmail.com>
2017-01-29 20:22:13 -0500

    AddedLabAssingment1

---
+++ LabAssignment1/Lab Assignment 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string middleInitial;
+            string lastName;
+            string fullName;
+            int age;
+            bool isCitizen;
+            bool canVote;
+            int heightFeet;
+            double heightInches;
+            double totalHeightCM;
+
+            firstName = "Brandon";
+            middleInitial = "C.";
+            lastName = "Wright";
+            fullName = "Brandon Curtis Wright";
+            age = 19;
+            isCitizen = true;
+            canVote = true;
+            heightFeet = 6;
+            heightInches = 3;
+            totalHeightCM = 190.5;
+
+        }
+    }
+}
