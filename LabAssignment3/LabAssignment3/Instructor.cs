fbef99b0df8bc56a1bbe40a02759c8ffea75e5dd
brandon wright <brandoncwright2@gmail.com>
2017-03-22 19:58:36 -0400

    Lab 3 finished

---
+++ LabAssignment3/LabAssignment3/Instructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabAssignment3
+{
+    class Instructor
+    {
+        public string Name;
+        public string CourseName;
+
+        public Instructor(string name, string course)
+        {
+            this.Name = name;
+            this.CourseName = course;
+        }
+        public void SetStudentGrade(Student student, int grade)
+        {
+            student.SetGrade(grade);
+        }
+
+        public void PrintInstructorInfo()
+        {
+            System.Console.WriteLine("Instructor", this.Name);
+            System.Console.WriteLine("Course", this.CourseName);
+
+        }
+         
+    }
+}
---
+++ LabAssignment3/LabAssignment3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace LabAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Instructor("John", "English");
+            var mike = new Instructor("Mike", "Math");
+
+            var jane = new Student("Jane", john);
+            var joe = new Student("Joe", john);
+            var melissa = new Student("Melissa", mike);
+            var matt = new Student("Matt", mike);
+
+            john.SetStudentGrade(jane, 95);
+            john.SetStudentGrade(joe, 85);
+            mike.SetStudentGrade(melissa, 90);
+            mike.SetStudentGrade(matt, 92);
+
+            john.PrintInstructorInfo();
+            mike.PrintInstructorInfo();
+
+            jane.PrintStudentInfo();
+            joe.PrintStudentInfo();
+            melissa.PrintStudentInfo();
+            matt.PrintStudentInfo();
+
+            System.Console.ReadKey();
+
+           
+        }
+    }
+}
